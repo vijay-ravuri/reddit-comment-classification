f5861a2df1ae65996a067ea221ecd870c8a70927
Vijay Ravuri <vijay_ravuri@outlook.com>
2023-09-21 15:55:49 -0700

    some restructuring and grid searching

---
+++ code/text_helper.py
@@ -0,0 +1,26 @@
+import string
+import re
+import numpy as np
+import emoji
+from nltk.corpus import stopwords
+punctuation = string.punctuation
+
+def word_splitter(text):  # Number of words + average word length
+    words = re.split(r'[\s\/]', re.sub(r'[\.]', '', text))
+    return len(words), np.mean([len(word) for word in words])
+
+
+def sentence_count(text): # Number of sentences + average sentence length (in words)
+    sentences = re.split(r'\.\s', text)
+    return len(sentences), np.mean([len(re.split(r'[\s\/]', sentence)) for sentence in sentences])
+
+def emoji_checker(text): # Checks for presence of any emojis
+    if len(emoji.emoji_list(text)) > 0:
+        return True
+    return False
+
+def stop_word_counter(text): # Number of stopwords
+    return sum([text.count(word) for word in stopwords.words('english')])
+
+def punc_counter(text): # Counts the punctuation in text input
+    return sum([text.count(punc) for punc in punctuation])
